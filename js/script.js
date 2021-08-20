cec4bc144078224554e47edee0f712a8f175ef3d
ankitkumarsharma <aksharma.dixit@gmail.com>
2021-08-20 22:03:56 +0530

    java script initial

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function changeData(value){  // parameters
+    var treatWithFriends = "First give me treat then only u can go with your partner!!!"; // data types with it's value.
+    var xyz = "str" + 45;
+    console.log(value);
+    window.alert("Happy Wedding dear "+ value +"," + treatWithFriends);
+    document.getElementById("test").innerHTML = value;
+}
