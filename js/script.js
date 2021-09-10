b008bb8cf7aa91bb03069e4929967e37fd2e2491
ankitkumarsharma <aksharma.dixit@gmail.com>
2021-09-10 21:41:44 +0530

    functions & variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 function changeData(value){  // parameters
     var treatWithFriends = "First give me treat then only u can go with your partner!!!"; // data types with it's value.
     var xyz = "str" + 45;
@@ -5,3 +7,36 @@ function changeData(value){  // parameters
     window.alert("Happy Wedding dear "+ value +"," + treatWithFriends);
     document.getElementById("test").innerHTML = value;
 }
+
+var abc = [2,3,4,5,6] // global scope variable
+function cube(num){
+    return num*num*num;   // function return
+}
+function sum(num){    // function declare with parameters
+    num = num + 1;
+    console.log(num);
+    return num;
+}
+//-------------------------
+// let & const --- block level scope variable
+
+function esStart(){
+    let inputValue =  document.getElementById("num1").value;
+    document.getElementById("cube").innerHTML = cube(inputValue);
+    console.log(cube(inputValue)); 
+    sum(inputValue); // function calling/invocation
+    document.getElementById("sum").innerHTML = sum(inputValue);
+
+    if(true){
+        let firstName = "Ankit";
+        // firstName = "aaaaa";
+        const lastName = "Sharma";
+        // lastName = "hsachsd";  // we can't reassign value in const
+        var otherName = "Sumit";
+        console.log(firstName, lastName, otherName);
+    }
+    // console.log(firstName, lastName, otherName);
+}
+
+
+
